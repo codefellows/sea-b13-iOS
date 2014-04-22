58ee9f5b27ac10c9ba8e02e9163370e3129bd6d4
bpj823 <bradleypj@icloud.com>
2014-04-22 12:03:42 -0700

    added oauth

--- githubtogoCF/githubtogoCF/CFAppDelegate.m
+++ githubtogoCF/githubtogoCF/CFAppDelegate.m
@@ -12,10 +12,19 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.networkController = [CFNetworkController new];
     // Override point for customization after application launch.
     return YES;
 }
-							
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    [self.networkController handleOAuthCallbackWithURL:url];
+    return YES;
+}
+
+
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
