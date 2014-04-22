90988345eb3b962618fcadacdd10982e326718a5
bpj823 <bradleypj@icloud.com>
2014-04-22 12:05:05 -0700

    added Oauth2

---
+++ Github To Go/Github To Go/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Github To Go
+//
+//  Created by John Clem on 4/21/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ Github To Go/Github To Go/Repo.m
@@ -0,0 +1,18 @@
+//
+//  Repo.m
+//  Github To Go
+//
+//  Created by John Clem on 4/21/14.
+//  Copyright (c) 2014 Code Fellows. All rights reserved.
+//
+
+#import "Repo.h"
+
+
+@implementation Repo
+
+@dynamic html_cache;
+@dynamic html_url;
+@dynamic name;
+
+@end
