547eda94c223b9c3d4833a0018b78409df07e122
bpj823 <bradleypj@icloud.com>
2014-05-08 09:13:26 -0700

    added fetched results controller

--- coredataexample/coredataexample/CFViewController.m
+++ coredataexample/coredataexample/CFViewController.m
@@ -11,11 +11,13 @@
 #import "Label.h"
 #import "CFArtistsViewController.h"
 
-@interface CFViewController () < UITableViewDelegate,UITableViewDataSource>
+@interface CFViewController () < UITableViewDelegate,UITableViewDataSource,NSFetchedResultsControllerDelegate>
 
 @property (weak,nonatomic) NSManagedObjectContext *objectContext;
-@property (strong,nonatomic) NSArray *labels;
+//@property (strong,nonatomic) NSArray *labels;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+
 
 @end
 
@@ -58,14 +60,40 @@
     
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Label"];
     
-    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"any artists.genre = %@",@"Awesome"];
+    //fetchRequest.predicate = [NSPredicate predicateWithFormat:@"any artists.genre = %@",@"Awesome"];
     NSError *error;
     
-    self.labels = [self.objectContext executeFetchRequest:fetchRequest error:&error];
+    //self.labels = [self.objectContext executeFetchRequest:fetchRequest error:&error];
     
-    NSLog(@" count: %lu",(unsigned long)self.labels.count);
+    //NSLog(@" count: %lu",(unsigned long)self.labels.count);
     
+    //fetchRequest setFetchBatchSize:20];
+    
+    // Edit the sort key as appropriate.
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:NO];
+    NSArray *sortDescriptors = @[sortDescriptor];
+    
+    //people = [self.managedObjectContext executeFetchRequest:fetchRequest error:nil];
+    
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    //NSArray *array = [self.objectContext executeFetchRequest:fetchRequest error:nil];
+    
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.objectContext sectionNameKeyPath:nil cacheName:nil];
+    aFetchedResultsController.delegate = self;
+    self.fetchedResultsController = aFetchedResultsController;
+    self.fetchedResultsController.delegate = self;
+    
+   BOOL worked =  [self.fetchedResultsController performFetch:nil];
     [self.tableView reloadData];
+    
+    NSLog(@" %hhd",worked);
+//	if (![self.fetchedResultsController performFetch:&error]) {
+//        // Replace this implementation with code to handle the error appropriately.
+//        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//	    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//	    abort();
+//	}
 
 }
 
@@ -75,29 +103,89 @@
     {
         CFArtistsViewController *destinationVC = segue.destinationViewController;
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        destinationVC.selectedLabel = self.labels[indexPath.row];
+        //destinationVC.selectedLabel = self.labels[indexPath.row];
     }
 }
 
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [[self.fetchedResultsController sections] count];
+}
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.labels.count;
+    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][section];
+    return [sectionInfo numberOfObjects];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"labelCell"];
     
-    Label *label = self.labels[indexPath.row];
-    cell.textLabel.text = label.name;
+    //NSManagedObject *object = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    [self configureCell:cell atIndexPath:indexPath];
+   
     
     return cell;
 }
 
-- (void)didReceiveMemoryWarning
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSManagedObject *object = [self.fetchedResultsController objectAtIndexPath:indexPath];
+     cell.textLabel.text = [object valueForKey:@"name"];
+}
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+    // The fetch controller is about to start sending change notifications, so prepare the table view for updates.
+    [self.tableView beginUpdates];
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+    
+    UITableView *tableView = self.tableView;
+    
+    switch(type) {
+            
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:[NSArray
+                                               arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:[NSArray
+                                               arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id )sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+    
+    switch(type) {
+            
+        case NSFetchedResultsChangeInsert:
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    // The fetch controller has sent all current change notifications, so tell the table view to process all updates.
+    [self.tableView endUpdates];
 }
 
 @end
